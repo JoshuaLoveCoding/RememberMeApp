5ab0910b1e7dd62d2414ce34e0e713139488ef4e
JoshuaLoveCoding <anqi6591@gwu.edu>
2018-12-25 20:21:15 -0500

    Add Navigation Bottom Bar

---
+++ app/src/main/java/edu/gwu/rememberme/MenuActivity.kt
@@ -0,0 +1,29 @@
+package edu.gwu.rememberme
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_menu.*
+
+class MenuActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.navigation_notifications -> {
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
